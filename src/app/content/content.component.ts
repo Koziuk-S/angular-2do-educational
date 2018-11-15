8cc52d965ea2548900d354ac684a1e5294f94e2a
Serhiy Koziuk <serhiy.koziuk@eleks.com>
2018-11-15 16:12:08 +0200

    step-2

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  public todos: Array<ITodo>;
 
-  constructor() { }
+  constructor() {
+    this.todos = _todos;
+  }
 
   ngOnInit() {
   }
 
 }
+
+interface ITodo {
+  done: boolean;
+  title: string;
+  description: string;
+  isDescription: boolean;
+}
+
+const _todos: Array<ITodo> = [
+  <ITodo>{
+    done: true,
+    title: 'Learn HTML',
+    description: ''
+  },
+  <ITodo>{
+    done: false,
+    title: 'Learn SCC',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, nobis.'
+  },
+  <ITodo>{
+    done: false,
+    title: 'Learn JS',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, nobis.'
+  }
+];
