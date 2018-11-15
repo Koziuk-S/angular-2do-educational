080fb11f9013aa8a4f043ba21265ef6eeb7eeee7
Serhiy Koziuk <serhiy.koziuk@eleks.com>
2018-11-15 16:02:45 +0200

    step-1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  public headerTitle: string;
+  public subTitle: string;
+
+  constructor() {
+    this.headerTitle = 'Angular 2Do';
+    this.subTitle = 'Educational project for study Angular.';
+  }
+
+  ngOnInit() {
+  }
+
+}
