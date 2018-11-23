40ad8a3ed9a1db7ac63123bfa82cfc7bf612d946
Serhiy Koziuk <serhiy.koziuk@eleks.com>
2018-11-23 12:33:31 +0200

    clear form

--- src/app/content/new-todo/new-todo.component.ts
+++ src/app/content/new-todo/new-todo.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { NewTodo } from 'src/app/shared/classes';
 import { ITodo } from 'src/app/shared/interfaces';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-todo',
@@ -11,6 +12,8 @@ export class NewTodoComponent implements OnInit {
   public title: string;
   public description: string;
 
+  @ViewChild('newToDoForm') newToDoForm: FormGroup;
+
   @Output() submitNewTodo: EventEmitter<ITodo> = new EventEmitter();
 
   constructor() { }
@@ -20,9 +23,7 @@ export class NewTodoComponent implements OnInit {
 
   public onSubmit(): void {
     this.submitNewTodo.emit(new NewTodo(this.title, this.description));
-
-    this.title = '';
-    this.description = '';
+    this.newToDoForm.reset();
   }
 
 }
